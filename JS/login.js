cb16d71dde657f4e96cb116f8f9bd611f71998d2
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-06-21 22:12:10 -0300

    iniciar tela de cadastro

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,15 @@
-function telalogin(){
+    var telaprincipal = document.getElementById("iprinc")
+    var telalogin = document.getElementById("iform")
+
+    function exibirOcultarLogin(){
+        if (telaprincipal.style.display === "none"){
+            telaprincipal.style.display = "block"
+            telalogin.style.display = "none"
+        } else{
+            telaprincipal.style.display = "none"
+            telalogin.style.display = "block"
+        }
+    
+
     
 }
