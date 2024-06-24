2c706e2ef2ef1e4a0d45a573dfe71281270edc8a
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-06-24 18:10:49 -0300

    Ajuste  nos botoes de esqueci senha

--- JS/login.js
+++ JS/login.js
@@ -1,13 +1,17 @@
 var telaprincipal = document.getElementById("iprinc")
 var telalogin = document.getElementById("iform")
+var telaRecuperarSenha = document.getElementById("iform2")
+var esqueciSenha = document.getElementById("irecuperarSenha")
 
 function exibirOcultarLogin(){
      if (telaprincipal.style.display === "none"){
             telaprincipal.style.display = "block"
             telalogin.style.display = "none"
+            
+
     } else{
             telaprincipal.style.display = "none"
             telalogin.style.display = "block"
-    }
+        
+     }
 }
-
