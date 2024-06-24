ea58b87dbc77547a9fa60899f69b3be27f8823ce
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-06-24 15:59:43 -0300

    Finalização estilo recuperação de senha

--- JS/login.js
+++ JS/login.js
@@ -1,15 +1,13 @@
-    var telaprincipal = document.getElementById("iprinc")
-    var telalogin = document.getElementById("iform")
+var telaprincipal = document.getElementById("iprinc")
+var telalogin = document.getElementById("iform")
 
-    function exibirOcultarLogin(){
-        if (telaprincipal.style.display === "none"){
+function exibirOcultarLogin(){
+     if (telaprincipal.style.display === "none"){
             telaprincipal.style.display = "block"
             telalogin.style.display = "none"
-        } else{
+    } else{
             telaprincipal.style.display = "none"
             telalogin.style.display = "block"
-        }
-    
-
-    
+    }
 }
+
