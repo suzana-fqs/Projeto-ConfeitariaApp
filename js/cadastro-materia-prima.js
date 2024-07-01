88f5508b716ee4e871eadcb3f46d57fa9e08e65a
suzana-fqs <147282124+suzana-fqs@users.noreply.github.com>
2024-07-01 15:49:52 -0300

    Aplicando lógica do cadastro de materia prima

---
+++ js/cadastro-materia-prima.js
@@ -0,0 +1,8 @@
+const MateriaPrima = document.getElementById("iNomeMateriaPrima")
+const Quantidade = document.getElementById("iQtdMateriaPrima")
+const Unidade = document.getElementById("iunidade")
+const Preco = document.getElementById("iPrecoMateriaPrima")
+
+function cadastrarMateriaPrima(){
+
+}
